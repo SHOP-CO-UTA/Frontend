ab64c7cd9cd29d9f8aa6980931a30ba8c2b5b632
NguyenHoaiPhong <nguyenhoaiphong14723@gmail.com>
2026-04-23 11:12:01 +0700

    Update page.tsx

--- src/app/(blog)/blog/[blogID]/page.tsx
+++ src/app/(blog)/blog/[blogID]/page.tsx
@@ -1,7 +1,12 @@
-export default function BlogPost({ params }: { params: { blogID: string } }) {
+export default async function BlogPost({
+    params,
+}: {
+    params: Promise<{ blogID: string }>;
+}) {
+    const { blogID } = await params;
     return (
         <div>
-            <h1>Blog Post {params.blogID}</h1>
+            <h1>Blog Post {blogID}</h1>
             <p>This is a blog post.</p>
         </div>
     );
